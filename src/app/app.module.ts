6a2b77f7fac7120dffbcd2c3a169e6ca4f8c034e
Alex <oleksii.ihnatiev@pl.abb.com>
2021-01-04 12:29:09 +0100

    Clean Index html |&| Install Angular material |&| Add Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from './materials.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/** @title Sidenav with configurable mode */
+@Component({
+  selector: 'app-navbar',
+  templateUrl: 'navbar.component.html',
+  styleUrls: ['navbar.component.scss'],
+})
+export class NavbarComponent {
+  
+}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
